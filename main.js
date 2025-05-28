8d17112a707f73c4be9b077174029fd9f031996c
Lin_tsen <88596287+Lintsen3847@users.noreply.github.com>
2025-05-29 03:33:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         次元城動畫播放器淨化
+// @name         次元城優化
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.5
 // @description  隱藏影片進度條，阻止開發者工具跳轉百度
 // @author       Lin_tsen
 // @match        https://www.ciyuancheng.net/watch/*
@@ -12,36 +12,16 @@
 (function() {
     'use strict';
 
-    // 阻止跳轉百度的防偵錯腳本
-    const originalDebug = window.debugger;
-    Object.defineProperty(window, 'debugger', {
-        get: function () {
-            return function () {}; // 覆寫為空
-        },
-        set: function () {}
-    });
-
-    // 阻止 setInterval偵測開發者工具
+    // 嘗試攔截 setInterval 裡含 debugger 且有 baidu.com 的函數，阻止執行
     const originalSetInterval = window.setInterval;
-    window.setInterval = function(fn, delay) {
+    window.setInterval = function(fn, delay, ...args) {
         const fnStr = fn.toString();
         if (fnStr.includes('debugger') && fnStr.includes('baidu.com')) {
             console.log('Blocked setInterval with debugger + baidu redirect');
             return null;
         }
-        return originalSetInterval(fn, delay);
+        return originalSetInterval(fn, delay, ...args);
     };
-
-    // 當頁面載入完成後隱藏 mini-progress 元素
-    window.addEventListener('DOMContentLoaded', () => {
-        const style = document.createElement('style');
-        style.innerHTML = `
-            .mini-progress {
-                display: none !important;
-            }
-        `;
-        document.head.appendChild(style);
-    });
 
 })();
 
